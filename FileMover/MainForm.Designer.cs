e8f1812671219aace349c04bd76e0aaba90f5319
CyberAndrey77 <lapardin.andrey@mail.ru>
2021-07-20 15:10:20 +0800

    Исправлены ошибки при создании папки, при удалении из списка файлов.

--- FileMover/MainForm.Designer.cs
+++ FileMover/MainForm.Designer.cs
@@ -38,14 +38,21 @@ namespace FileMover
             this.moverButton = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.selectFileButton = new System.Windows.Forms.Button();
-            this.selectedFilesRichTextBox = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.selectMaskFileButton = new System.Windows.Forms.Button();
             this.selectMaskFileTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.selectFilesByMaskRadioButton = new System.Windows.Forms.RadioButton();
+            this.selectFilesInExplorerRadioButton = new System.Windows.Forms.RadioButton();
+            this.selectMaskFileLabel = new System.Windows.Forms.Label();
+            this.selectedFilesListBox = new System.Windows.Forms.ListBox();
+            this.removePathFileButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // sourcePathExplorerButton
             // 
+            this.sourcePathExplorerButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.sourcePathExplorerButton.Location = new System.Drawing.Point(648, 58);
             this.sourcePathExplorerButton.Name = "sourcePathExplorerButton";
             this.sourcePathExplorerButton.Size = new System.Drawing.Size(120, 28);
@@ -74,6 +81,7 @@ namespace FileMover
             // 
             // finalPathExplorerButton
             // 
+            this.finalPathExplorerButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.finalPathExplorerButton.Location = new System.Drawing.Point(648, 142);
             this.finalPathExplorerButton.Name = "finalPathExplorerButton";
             this.finalPathExplorerButton.Size = new System.Drawing.Size(120, 28);
@@ -98,7 +106,7 @@ namespace FileMover
             // 
             // moverButton
             // 
-            this.moverButton.Location = new System.Drawing.Point(648, 240);
+            this.moverButton.Location = new System.Drawing.Point(648, 592);
             this.moverButton.Name = "moverButton";
             this.moverButton.Size = new System.Drawing.Size(120, 28);
             this.moverButton.TabIndex = 6;
@@ -108,7 +116,7 @@ namespace FileMover
             // 
             // selectFileButton
             // 
-            this.selectFileButton.Location = new System.Drawing.Point(51, 240);
+            this.selectFileButton.Location = new System.Drawing.Point(51, 383);
             this.selectFileButton.Name = "selectFileButton";
             this.selectFileButton.Size = new System.Drawing.Size(120, 28);
             this.selectFileButton.TabIndex = 7;
@@ -116,21 +124,13 @@ namespace FileMover
             this.selectFileButton.UseVisualStyleBackColor = true;
             this.selectFileButton.Click += new System.EventHandler(this.selectFileButton_Click);
             // 
-            // selectedFilesRichTextBox
-            // 
-            this.selectedFilesRichTextBox.Location = new System.Drawing.Point(51, 275);
-            this.selectedFilesRichTextBox.Name = "selectedFilesRichTextBox";
-            this.selectedFilesRichTextBox.Size = new System.Drawing.Size(570, 163);
-            this.selectedFilesRichTextBox.TabIndex = 8;
-            this.selectedFilesRichTextBox.Text = "";
-            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // selectMaskFileButton
             // 
-            this.selectMaskFileButton.Location = new System.Drawing.Point(200, 240);
+            this.selectMaskFileButton.Location = new System.Drawing.Point(376, 283);
             this.selectMaskFileButton.Name = "selectMaskFileButton";
             this.selectMaskFileButton.Size = new System.Drawing.Size(182, 28);
             this.selectMaskFileButton.TabIndex = 9;
@@ -140,19 +140,87 @@ namespace FileMover
             // 
             // selectMaskFileTextBox
             // 
-            this.selectMaskFileTextBox.Location = new System.Drawing.Point(402, 243);
+            this.selectMaskFileTextBox.Location = new System.Drawing.Point(376, 240);
             this.selectMaskFileTextBox.Name = "selectMaskFileTextBox";
-            this.selectMaskFileTextBox.Size = new System.Drawing.Size(219, 22);
+            this.selectMaskFileTextBox.Size = new System.Drawing.Size(182, 22);
             this.selectMaskFileTextBox.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.selectFilesByMaskRadioButton);
+            this.groupBox1.Controls.Add(this.selectFilesInExplorerRadioButton);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Location = new System.Drawing.Point(51, 197);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 168);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // selectFilesByMaskRadioButton
+            // 
+            this.selectFilesByMaskRadioButton.AutoSize = true;
+            this.selectFilesByMaskRadioButton.Location = new System.Drawing.Point(7, 69);
+            this.selectFilesByMaskRadioButton.Name = "selectFilesByMaskRadioButton";
+            this.selectFilesByMaskRadioButton.Size = new System.Drawing.Size(211, 21);
+            this.selectFilesByMaskRadioButton.TabIndex = 1;
+            this.selectFilesByMaskRadioButton.TabStop = true;
+            this.selectFilesByMaskRadioButton.Text = "Выбор файлов через маску";
+            this.selectFilesByMaskRadioButton.UseVisualStyleBackColor = true;
+            this.selectFilesByMaskRadioButton.CheckedChanged += new System.EventHandler(this.selectFilesByMaskRadioButton_CheckedChanged);
+            // 
+            // selectFilesInExplorerRadioButton
+            // 
+            this.selectFilesInExplorerRadioButton.AutoSize = true;
+            this.selectFilesInExplorerRadioButton.Location = new System.Drawing.Point(7, 22);
+            this.selectFilesInExplorerRadioButton.Name = "selectFilesInExplorerRadioButton";
+            this.selectFilesInExplorerRadioButton.Size = new System.Drawing.Size(219, 21);
+            this.selectFilesInExplorerRadioButton.TabIndex = 0;
+            this.selectFilesInExplorerRadioButton.TabStop = true;
+            this.selectFilesInExplorerRadioButton.Text = "Выбор файлов в проводнике";
+            this.selectFilesInExplorerRadioButton.UseVisualStyleBackColor = true;
+            this.selectFilesInExplorerRadioButton.CheckedChanged += new System.EventHandler(this.selectFilesInExplorerRadioButton_CheckedChanged);
+            // 
+            // selectMaskFileLabel
+            // 
+            this.selectMaskFileLabel.AutoSize = true;
+            this.selectMaskFileLabel.Location = new System.Drawing.Point(376, 197);
+            this.selectMaskFileLabel.Name = "selectMaskFileLabel";
+            this.selectMaskFileLabel.Size = new System.Drawing.Size(152, 17);
+            this.selectMaskFileLabel.TabIndex = 12;
+            this.selectMaskFileLabel.Text = "Введите маску файла";
+            // 
+            // selectedFilesListBox
+            // 
+            this.selectedFilesListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.selectedFilesListBox.FormattingEnabled = true;
+            this.selectedFilesListBox.IntegralHeight = false;
+            this.selectedFilesListBox.ItemHeight = 16;
+            this.selectedFilesListBox.Location = new System.Drawing.Point(51, 429);
+            this.selectedFilesListBox.Name = "selectedFilesListBox";
+            this.selectedFilesListBox.Size = new System.Drawing.Size(570, 191);
+            this.selectedFilesListBox.TabIndex = 13;
+            // 
+            // removePathFileButton
+            // 
+            this.removePathFileButton.Location = new System.Drawing.Point(648, 429);
+            this.removePathFileButton.Name = "removePathFileButton";
+            this.removePathFileButton.Size = new System.Drawing.Size(120, 28);
+            this.removePathFileButton.TabIndex = 14;
+            this.removePathFileButton.Text = "Удалить путь";
+            this.removePathFileButton.UseVisualStyleBackColor = true;
+            this.removePathFileButton.Click += new System.EventHandler(this.removePathFileButton_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 632);
+            this.Controls.Add(this.removePathFileButton);
+            this.Controls.Add(this.selectedFilesListBox);
+            this.Controls.Add(this.selectMaskFileLabel);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.selectMaskFileTextBox);
             this.Controls.Add(this.selectMaskFileButton);
-            this.Controls.Add(this.selectedFilesRichTextBox);
             this.Controls.Add(this.selectFileButton);
             this.Controls.Add(this.moverButton);
             this.Controls.Add(this.finalPathTextBox);
@@ -161,9 +229,12 @@ namespace FileMover
             this.Controls.Add(this.finalPathLabel);
             this.Controls.Add(this.sourcePathLabel);
             this.Controls.Add(this.sourcePathExplorerButton);
+            this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(818, 497);
             this.Name = "MainWindow";
             this.Text = "FileMover";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -180,10 +251,15 @@ namespace FileMover
         private System.Windows.Forms.Button moverButton;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.Button selectFileButton;
-        private System.Windows.Forms.RichTextBox selectedFilesRichTextBox;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button selectMaskFileButton;
         private System.Windows.Forms.TextBox selectMaskFileTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton selectFilesByMaskRadioButton;
+        private System.Windows.Forms.RadioButton selectFilesInExplorerRadioButton;
+        private System.Windows.Forms.Label selectMaskFileLabel;
+        private System.Windows.Forms.ListBox selectedFilesListBox;
+        private System.Windows.Forms.Button removePathFileButton;
     }
 }
 
---
+++ FileMover/MainForm.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FileMover
+{
+    public partial class MainWindow : Form
+    {
+        private List<string> _filePaths = new List<string>();
+        private bool _isSelectFilesInExplorer;
+        private string _fileMask;
+        private bool _isSelectFilesByMask;
+        public MainWindow()
+        {
+            InitializeComponent();
+            selectFilesInExplorerRadioButton.Checked = true;
+
+        }
+
+        private void SearchDirectoryPath(Control textBox)
+        {
+            var result = folderBrowserDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                textBox.Text = folderBrowserDialog1.SelectedPath;
+            }
+            return;
+        }
+
+        /// <summary>
+        /// Добавляет пути к файлам в ListBox и в _fileList.
+        /// </summary>
+        /// <param name="files"> пути к файлам в строковом массиве</param>
+        private void AddToLists(string[] files)
+        {
+            foreach (var file in files)
+            {
+                if (!selectedFilesListBox.Items.Contains(file))
+                {
+                    selectedFilesListBox.Items.Add(file);
+                    _filePaths.Add(file);
+                }
+            }
+        }
+
+        private void sourcePathExplorerButton_Click(object sender, EventArgs e)
+        {
+            sourcePathTextBox.ForeColor = Color.Black;
+            SearchDirectoryPath(sourcePathTextBox);
+        }
+
+        private void finalPathExplorerButton_Click(object sender, EventArgs e)
+        {
+            SearchDirectoryPath(finalPathTextBox);
+        }
+
+        private void moverButton_Click(object sender, EventArgs e)
+        {
+            if (_filePaths.Count == 0)
+            {
+                return;
+            }
+
+            var targetDirectoryPath = finalPathTextBox.Text;
+            if (targetDirectoryPath == string.Empty)
+            {
+                MessageBox.Show
+                (
+                   "Не указан путь куда перенести файлы!",
+                   "Не выбран путь!",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Exclamation
+                );
+                return;
+            }
+
+            if (targetDirectoryPath == sourcePathTextBox.Text)
+            {
+                MessageBox.Show
+                (
+                   "Пути совпадают!",
+                   "Ошибка путей!",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Exclamation
+                );
+                return;
+            }
+
+            try
+            {
+                if (!System.IO.Directory.Exists(targetDirectoryPath))
+                {
+                    var result = MessageBox.Show
+                    (
+                        "Не удаётся найти папку, создать папку?",
+                        "Нет папки!",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Exclamation
+                     );
+
+                    if (result == DialogResult.Yes)
+                    {
+                        System.IO.Directory.CreateDirectory(targetDirectoryPath);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+
+                foreach (var filePath in _filePaths)
+                {
+                    var fileName = System.IO.Path.GetFileName(filePath);
+                    var targetFile = System.IO.Path.Combine(targetDirectoryPath, fileName);
+
+                    System.IO.File.Move(filePath, targetFile);
+                }
+                _filePaths.Clear();
+                selectedFilesListBox.Items.Clear();
+
+                MessageBox.Show("Файлы успешно перенесены!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void selectFileButton_Click(object sender, EventArgs e)
+        {
+            if (_isSelectFilesByMask)
+            {
+                string[] files = System.IO.Directory.GetFiles(sourcePathTextBox.Text, _fileMask);
+                AddToLists(files);
+                return;
+            }
+
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.InitialDirectory = sourcePathTextBox.Text;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string[] files = openFileDialog1.FileNames;
+                AddToLists(files);
+            }
+        }
+
+        /// <summary>
+        /// Записывает маску файла в переменную.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void selectMaskFileButton_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(sourcePathTextBox.Text))
+            {
+                MessageBox.Show
+                (
+                   "Не указан путь откуда перенести файлы!",
+                   "Не выбран путь!",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Exclamation
+                );
+                return;
+            }
+
+            _fileMask = selectMaskFileTextBox.Text;
+        }
+
+        private void selectFilesInExplorerRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            _isSelectFilesInExplorer = true;
+            _isSelectFilesByMask = false;
+            selectMaskFileButton.Enabled = false;
+            selectMaskFileTextBox.Enabled = false;
+        }
+
+        private void selectFilesByMaskRadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            _isSelectFilesInExplorer = false;
+            _isSelectFilesByMask = true;
+            selectMaskFileButton.Enabled = true;
+            selectMaskFileTextBox.Enabled = true;
+        }
+
+        /// <summary>
+        /// Удаляет путь из списков ListBox и _filePath.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void removePathFileButton_Click(object sender, EventArgs e)
+        {
+            if (selectedFilesListBox.SelectedIndex < 0)
+            {
+                return;
+            }
+
+            _filePaths.RemoveAt(selectedFilesListBox.SelectedIndex);
+            selectedFilesListBox.Items.RemoveAt(selectedFilesListBox.SelectedIndex);
+        }
+    }
+}
